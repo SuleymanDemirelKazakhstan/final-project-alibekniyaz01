d33c2ccd648ba031c16f3db6543ba93f209cc918
alibekniyaz01 <190107074@stu.sdu.edu.kz>
2020-12-19 13:04:39 +0600

    final project update

--- finalProject1.com/www/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div>SDU 2020</div>
-    <div>Created and Designed by Bekniyaz Alibek </div>
-</footer>
-
-<!-- </div> -->
-
-<link rel="stylesheet" href="./style/footerStyle.css">
